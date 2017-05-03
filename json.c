01b0a1ed7e2b589031385bcf3cd5d82821dbbfd4
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-05-04 01:49:21 +0700

    .

--- json.c
+++ json.c
@@ -3,7 +3,9 @@
 #include "main.h"
 #define SIZEBUFFER MINSIZE*1
 //shitcode228
-
+#define Check(buffer) if(*buffer != '"' || *(buffer+1) != ':' || *(buffer+2) != '"' && *buffer) return 0;\
+ buffer+=3;
+#define Check1(buffer) if(*buffer != '"' || *(buffer+1) != ',' || *(buffer+2) != '"' && *buffer) return 0;
 char * getOnlyJson(char*buffer)
 {
 
@@ -32,6 +34,21 @@ buffer+=12;
 free(first);
 }
 
+int getP(char*buffer,char*data)
+{
+int count=0;
+while(*buffer != '"' && *buffer)
+
+{
+count++;
+*data=*buffer;
+*data++;
+*buffer++;
+}
+if(!*buffer) return 0;
+return count;
+}
+
 char ** getWork(char*buf)
 {
 char * buffer = getOnlyJson(buf);
@@ -46,6 +63,30 @@ while(
 || *(buffer+3) != 't' 
 || *(buffer+4) != 'a'  
 ) *buffer++;
+if(*buffer != '"')return 0;
+buffer+=7;
+if(*buffer != '"') return 0;
+*buffer++;
+char * data = (char*)malloc(sizeof(char) * strlen(buffer));
+buffer+=getP(buffer,data);
+Check1(buffer);
+buffer+=2;
+if(*(buffer) != '"' ||  *(buffer+1) != 'h' ||  *(buffer+2) != 'a' ||  *(buffer+3) != 's' ||  *(buffer+4) != 'h' ||  *(buffer+5) != '1' || *(buffer+6) != '"' && *buffer) return 0;
+buffer+=6;
 
+Check(buffer);
+
+char * hash1 = (char*)malloc(sizeof(char) * strlen(buffer));
+buffer+=getP(buffer,hash1);
+Check1(buffer);
+buffer+=2;
+
+if(*buffer && *buffer != '"' || *(buffer+1) != 't' || *(buffer+2) != 'a' || *(buffer+3) != 'r' || *(buffer+4) != 'g' || *(buffer+5) != 'e' || *(buffer+6) != 't' || *(buffer+7) != '"') return 0;
+buffer+=7;
+
+Check(buffer);
+char * target = (char*)malloc(sizeof(char) * strlen(buffer));
+buffer+=getP(buffer,target);
+printf("\ndata:%s\nhash1:%s\ntarget:%s\n",data,hash1,target);
 free(first);
 }
